bda52a1071a21fc575a3fd49d81366684f61ceef
Eloagbawe <eloagbawe@gmail.com>
2022-10-28 14:31:12 +0100

    update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,7 +14,7 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu /data/
 sudo chgrp -R ubuntu /data/
 sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.google.com permanent;" /etc/nginx/sites-available/default
-sudo sed -i '/server_name _/a location /hbnb_static {alias /data/web_static/current; index index.html index.htm;}' /etc/nginx/sites-available/default
-sudo sed -i '/server_name _/a error_page 404 /404.html; location = /404.html {root /var/www/error/;internal; }' /etc/nginx/sites-available/default
+sudo sed -i '/server_name _;/a location /hbnb_static {alias /data/web_static/current; index index.html index.htm;}' /etc/nginx/sites-available/default
+sudo sed -i '/server_name _;/a error_page 404 /404.html; location = /404.html {root /var/www/error/;internal; }' /etc/nginx/sites-available/default
 sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";/" /etc/nginx/nginx.conf
 sudo service nginx restart
