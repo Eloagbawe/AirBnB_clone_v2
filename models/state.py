cb4089412d4058f32a248c2399f029841564f0cc
Eloagbawe <eloagbawe@gmail.com>
2022-11-13 00:14:42 +0100

    Improve engines

--- models/state.py
+++ models/state.py
@@ -3,6 +3,8 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from os import getenv
+import models
 
 
 class State(BaseModel, Base):
@@ -11,3 +13,24 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship('City', backref="state",
                           cascade="all, delete, delete-orphan")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """public getter method cities to return the list
+            of City objects from storage linked to the current State
+            """
+            storage_list = models.storage.all()
+            cities_list = []
+            result = []
+
+            for key in storage_list:
+                model_name = key.split('.')[0]
+                if model_name == 'City':
+                    cities_list.append(storage_list[key])
+
+            for city in cities_list:
+                if city.state_id == self.id:
+                    result.append(city)
+
+            return result
